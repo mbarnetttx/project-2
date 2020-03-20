cbe8b76b9833e62521d8f85fcfa2719fc5ae9cc8
Jennifer Moundanos <jmoundanos@gmail.com>
2020-03-19 20:54:17 -0500

    corrected schema

--- Unsolved/models/schema.sql
+++ Unsolved/models/schema.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS indoorLearning_db;
 CREATE DATABASE indoorLearning_db;
 
+USE indoorLearning_db;
+
 CREATE TABLE links(
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(100),
