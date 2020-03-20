5ff22c7cd3ffc7a770556e2b10a4677f9b51bd58
jmoundanos <40547927+jmoundanos@users.noreply.github.com>
2020-03-19 20:55:04 -0500

    Update schema.sql

--- Unsolved/models/schema.sql
+++ Unsolved/models/schema.sql
@@ -1,11 +1,14 @@
 DROP DATABASE IF EXISTS indoorLearning_db;
 CREATE DATABASE indoorLearning_db;
 
+USE indoorLearning_db;
+
 CREATE TABLE links(
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(100),
     category VARCHAR(20),
-    link VARCHAR(240)
+    link VARCHAR(240),
+    primary key(id)
 );
 
 DROP DATABASE IF EXISTS testdb;
