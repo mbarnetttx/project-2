a0a620db662e9e862d90cb5bddd7903654c33a2f
Meredith Barnett <meredithbarnettcontact@gmail.com>
2020-03-24 18:54:32 -0600

    got input form to work

--- Unsolved/models/index.js
+++ Unsolved/models/index.js
@@ -1,26 +1,4 @@
-
-// var mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-
-//   // Your port; if not 3306
-//   port: 3306,
-
-//   // Your username
-//   user: "root",
-
-//   // Your password
-//   password: "password",
-//   database: "indoorLearning_db"
-// });
-
-// connection.connect(function(err) {
-//   if (err) throw err;
-//   console.log("connected as id " + connection.threadId + "\n");
-// });
-
-("use strict");
+"use strict";
 
 
 var fs = require("fs");
