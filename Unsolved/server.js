96a3aaf6a7c9209c803b6ca638d3a7db03e48ccf
jmoundanos <40547927+jmoundanos@users.noreply.github.com>
2020-03-30 10:37:13 -0500

    Update server.js
    add db connection

--- Unsolved/server.js
+++ Unsolved/server.js
@@ -1,8 +1,24 @@
-// require("dotenv").config();
+require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
 var db = require("./models");
+
+var connection;
+if (process.env.JAWSDB_URL) {
+  // Database is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // Database is local
+  connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: 'password1',
+    database: 'indoorLearning_db'
+  })
+}
+connection.connect();
 
 var app = express();
 var PORT = process.env.PORT || 3000;
